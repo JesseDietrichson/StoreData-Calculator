caba7964986e8a4613bc1e75b46f68f860a940f5
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-10 22:36:54 -0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int answer = firstNumber * secondNumber;
+
+            Console.WriteLine($"The answer is {answer}");
+        }
+    }
+}
